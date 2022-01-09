8c4e933280105b0a936121fcb7ec94cf2ca9c69d
Anmol Jain <defini>
2022-01-09 12:31:59 +0530

    REAFACTOR: Better folder structure

--- Leave Tracker/VisualEffectBlur.swift
+++ Leave Tracker/VisualEffectBlur.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct VisualEffectBlur<Content: View>: View {
 	/// Defaults to .systemMaterial
 	var blurStyle: UIBlurEffect.Style
-
+	
 	/// Defaults to nil
 	var vibrancyStyle: UIVibrancyEffectStyle?
 	var content: Content
-
+	
 	init(
 		blurStyle: UIBlurEffect.Style = .systemMaterial,
 		vibrancyStyle: UIVibrancyEffectStyle? = nil,
@@ -24,28 +24,28 @@ struct VisualEffectBlur<Content: View>: View {
 		self.vibrancyStyle = vibrancyStyle
 		self.content = content()
 	}
-
+	
 	var body: some View {
 		Representable(
 			blurStyle: blurStyle,
 			vibrancyStyle: vibrancyStyle,
 			content: ZStack { content }
 		)
-		.accessibility(hidden: Content.self == EmptyView.self)
+			.accessibility(hidden: Content.self == EmptyView.self)
 	}
- }
+}
 
- // MARK: - Representable
- extension VisualEffectBlur {
+// MARK: - Representable
+extension VisualEffectBlur {
 	struct Representable<Content: View>: UIViewRepresentable {
 		var blurStyle: UIBlurEffect.Style
 		var vibrancyStyle: UIVibrancyEffectStyle?
 		var content: Content
-
+		
 		func makeUIView(context: Context) -> UIVisualEffectView {
 			context.coordinator.blurView
 		}
-
+		
 		func updateUIView(_ view: UIVisualEffectView, context: Context) {
 			context.coordinator.update(
 				content: content,
@@ -53,53 +53,53 @@ struct VisualEffectBlur<Content: View>: View {
 				vibrancyStyle: vibrancyStyle
 			)
 		}
-
+		
 		func makeCoordinator() -> Coordinator {
 			Coordinator(content: content)
 		}
 	}
- }
+}
 
- // MARK: - Coordinator
- extension VisualEffectBlur.Representable {
+// MARK: - Coordinator
+extension VisualEffectBlur.Representable {
 	class Coordinator {
 		let blurView = UIVisualEffectView()
 		let vibrancyView = UIVisualEffectView()
 		let hostingController: UIHostingController<Content>
-
+		
 		init(content: Content) {
 			hostingController = UIHostingController(rootView: content)
 			hostingController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 			hostingController.view.backgroundColor = nil
 			blurView.contentView.addSubview(vibrancyView)
-
+			
 			blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 			vibrancyView.contentView.addSubview(hostingController.view)
 			vibrancyView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 		}
-
+		
 		func update(content: Content, blurStyle: UIBlurEffect.Style, vibrancyStyle: UIVibrancyEffectStyle?) {
 			hostingController.rootView = content
-
+			
 			let blurEffect = UIBlurEffect(style: blurStyle)
 			blurView.effect = blurEffect
-
+			
 			if let vibrancyStyle = vibrancyStyle {
 				vibrancyView.effect = UIVibrancyEffect(blurEffect: blurEffect, style: vibrancyStyle)
 			} else {
 				vibrancyView.effect = nil
 			}
-
+			
 			hostingController.view.setNeedsDisplay()
 		}
 	}
- }
+}
 
- // MARK: - Content-less Initializer
- extension VisualEffectBlur where Content == EmptyView {
+// MARK: - Content-less Initializer
+extension VisualEffectBlur where Content == EmptyView {
 	init(blurStyle: UIBlurEffect.Style = .systemMaterial) {
 		self.init(blurStyle: blurStyle, vibrancyStyle: nil) {
 			EmptyView()
 		}
 	}
- }
+}
